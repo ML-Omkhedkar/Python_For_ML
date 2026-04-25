169b81373232648e434f1e1082cc37e88a1d4afd
Omkar Khedkar <omya.business@gmail.com>
2026-04-25 11:38:02 +0530

    Add files via upload

---
+++ Python Topics For ML/Data Types And Sring Slice/data_types.py
@@ -0,0 +1,20 @@
+# numbers
+a = 10 # int
+b = 10.4 # float
+# srings
+c = "Hello" # str
+# boolean
+d = True # bool
+e = False # bool
+# NoneType
+f = None # NoneType
+# complex
+j = 34j # complex
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(j))
---
+++ Python Topics For ML/Operators/Arithmetic_operators.py
@@ -0,0 +1,9 @@
+a = 20
+
+print(a + 20)
+print(a - 10)
+print(a * 20)
+print(a / 5)
+print(a // 5)
+print(a % 3)
+print(a ** 3)
