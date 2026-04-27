d5c897c432cc3abd63b7a036a71510d2612ad887
ML-Omkhedkar <omya.business@gmail.com>
2026-04-27 10:36:55 +0530

    Update input and output function examples

--- input and output/input_output.py
+++ input and output/input_output.py
@@ -1,2 +1,14 @@
-age = int(input( "Enter Your Age: "))
-print(age)
+# By default, the input() function returns a string value
+
+a = input("Enter your input: ")
+print(type(a),a)
+
+# We can convert the input to a different data type
+b = int(input("Enter your input: "))
+print(type(b),b)
+
+# We can also use float() to convert the input to a float data type
+c = float(input("Enter your input: "))
+print(type(c),c) 
+
+# input function get input from user and print function prints output
