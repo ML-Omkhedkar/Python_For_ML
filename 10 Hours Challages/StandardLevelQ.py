8b9bc91591d141755c84e6b98f8ddbd3ad5c5bb5
ML-Omkhedkar <omya.business@gmail.com>
2026-05-02 09:08:49 +0530

    Add 10 Hours Challenges progress

---
+++ 10 Hours Challages/StandardLevelQ.py
@@ -0,0 +1,83 @@
+# Write a program that:
+
+# Takes a list of numbers
+# Prints:
+# Total sum
+# Count of even & odd numbers
+# Maximum and minimum value
+
+
+
+# =========================================
+# im Writing this code without any help of ai i Hope i understand question corectly 
+
+# l = [1,2,3,4,5,6,7,8,9,10]
+# even = []
+# odd = []
+# for i in l:
+#     if i%2 == 0:
+#         even.append(i)
+#         print(i)
+# for i in l:
+#     if i%2 != 0:
+#         odd.append(i)
+#         print(i)    
+# print(f"Your Enven Numbers Lengh Is {len(even)}")    
+# print(f"Your Odd Numbers Lengh Is {len(odd)}")    
+
+# Q1 End Here
+
+# Question: Using output of Q1 Create a new list:
+# Only even numbers
+# Square each number
+
+# =========================================
+# Im triying My Best To Solve I hope this q is also i understand corectly
+
+# l = [1,2,3,4,5,6,7,8,9,10]
+# even = []
+# for i in l:
+#     if i%2 == 0:
+#         even.append(i)
+#         print(i)
+# print(even)
+
+# for i in even:
+#     print(i**2)
+
+# Q2 Ends here
+
+# Question:
+# From original list:
+# Count frequency of each number
+
+# =========================================
+
+l = [1,2,2,3,1,6,6,7,8,8,9,9,9,9,9,8,9]
+freq = {}
+
+for i in l:
+    if i in freq:
+        freq[i] += 1
+
+    else:
+        freq[i] = 1       
+print(freq)
+
+# Q3 Ends Here
+
+# Question:From list:
+# Print:Unique elements
+# Duplicate elements
+
+l = [1,2,2,3,1,6,6,7,8,8,9,9,9,9,9,8,9]
+unique = []
+copy = []
+
+for i in l:
+    if i in unique:
+        unique[i] =+ 1
+
+print(unique)
+
+print(copy)
